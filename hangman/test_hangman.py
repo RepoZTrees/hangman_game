5cddb0c1045d804e2c561b65669a4fcddc20d8fe
Vik <csimm3330.com>
2019-12-28 22:08:45 +0530

    First commit. Test file for hangman.py. Removed comments

---
+++ hangman/test_hangman.py
@@ -0,0 +1,89 @@
+from hangman import get_secret_word
+from hangman import hide_word
+
+def test_secret_word_no_punctuation():
+    with open ("/tmp/words.txt","w") as f:
+        for i in ["word'one", "word_two", "wordthree"]:
+            f.write(i+"\n")
+    selected_word = get_secret_word('/tmp/words.txt')
+    assert selected_word == "wordthree"
+
+def test_hide_word_empty():
+    sw = "elephant"
+    assert hide_word(sw,[]) == "________"
+
+def test_hide_word_first_guess():
+    sw = "elephant"
+    assert hide_word(sw, ['l']) == "_l______"
+
+def test_hide_word_second_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l']) == "ele_____"
+
+def test_hide_word_third_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l','p']) == "elep____"
+
+def test_hide_fourth_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l','p','h']) == "eleph___"
+
+def test_hide_fifth_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l','p','h','a']) == "elepha__"
+
+def test_hide_sixth_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l','p','h','a','n']) == "elephan_"
+
+def test_hide_seventh_guess():
+    sw = "elephant"
+    assert hide_word(sw,['e','l','p','h','a','n','t']) == "elephant"
+    
+
+
+    
+
+    
+    # assert hw(sw,[l])== _l______
+    # assert hw(sw,[e,l]) == ele_____
+    # assert hw(sw,)
+    # for i in []
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+"""
+def test_secret_word_atleast_five():
+    with open("/tmp/words.txt","w") as f:
+        for i in ["wo", "wor", "word", "bigword"]:
+            f.write(i+"\n")
+    selected_word = get_secret_word('/tmp/words.txt')
+    assert selected_word == "bigword"
+
+    
+def test_secret_word_lowercase():
+    with open("/tmp/words.txt","w") as f:
+        for i in ["Wording","wOrding","WORDING","wording"]:
+            f.write(i+"\n")
+    selected_word = get_secret_word('/tmp/words.txt')
+    assert selected_word == "wording"
+
+def test_secret_word_no_repeat():
+    with open("/tmp/words.txt","w") as f:
+        for i in ["disaster","recall","advise","national","infrastructure","shots","fired","federation","duress"]:
+            f.write(i+"\n")
+    l = []
+    for i in range(3):
+        l.append(get_secret_word('/tmp/words.txt'))
+    assert le(set(1)) == 3
+"""
