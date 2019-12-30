e51919a1abf6df96f75157896261e4b2aebd3718
Vik <csimm3330.com>
2019-12-30 11:49:25 +0530

    deleted/cleaned up comments

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -16,15 +16,8 @@ def get_secret_word(word_file="/usr/share/dict/words"):
     word = random.choice(good_words)
     return word.lower()
 
-#either use string or .join
-def hide_word(sw,guesses):
-    guessed = []
-    for secret_letter in sw:
-        if secret_letter in guesses:
-            guessed.append(guesses)
-            return guessed
-
 def hide_word(sw, guesses):
+    
     guessed = []
 
     for secret_letter in sw:
@@ -34,44 +27,3 @@ def hide_word(sw, guesses):
             guessed.append('_')
     guessed_str = ''.join(guessed) 
     return guessed_str
-
-"""
-def first_correct_(secret_word, guesses):
-    guess = []
-    for sw in secret_word:
-        if sw in guesses:
-            guess.append(sw)
-        return "_l______"
-
-def second_correct_guess(secret_word, guesses):
-    guess = []
-    for sw in secret_word:
-        if sw in guesses:
-            guess.append(sw)
-        return "ele_____"
-
-def third_correct_guess(secret_word, guesses):
-    guess = []
-    for sw in secret_word:
-        if sw in guesses:
-            guess.append(sw)
-        return "elep____"
-    
- """ 
-    
-#     word = "elephant"
-#     hw = []
-#     gl = []
-#     for sl in word:
-#         if gl in sl:
-#             hw.append()
-#         else:
-            
-            
-#     return hw
-#secret_word = "elephant"
-#def hide_word(secret):
-    
-
-    
-    
