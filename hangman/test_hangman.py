ee5ae76cd60ed97deef036c0b6f182bfa2fda52a
Vik <csimm3330.com>
2019-12-31 16:07:01 +0530

    Version1, final

--- hangman/test_hangman.py
+++ hangman/test_hangman.py
@@ -1,6 +1,15 @@
-from hangman import get_secret_word
-from hangman import hide_word
+import hangman
+#from hangman import get_secret_word
+#from hangman import hide_word
+#from hangman import get_status_message
 #from hangman import test_secret_word_atleast_five
+
+from hangman import get_secret_word, hide_word, get_status_message
+
+#You can either say 'from file_name import function_name' or call as 'filename.function_name inside the function' 
+#to check the coverage: (venv) vik@vik-VirtualBox:~/lycaeum/tdd/hangman$ pytest -v --cov=hangman test_hangman.py
+
+
 
 def test_secret_word_no_punctuation():
     with open ("/tmp/words.txt","w") as f:
@@ -63,3 +72,102 @@ def test_secret_word_no_repeat():
     for i in range(3):
         l.append(get_secret_word('/tmp/words.txt'))
     assert len(set(l)) == 3
+
+#***
+
+def test_status_message():
+    status = hangman.get_status_message(sw = 'elephant',
+                                        correct_guesses = ['e','l'],
+                                        wrong_guesses = ['x'],
+                                        turns_left = 4)
+    assert status == """ele_____
+Guesses:elx
+Turns left:4
+"""
+
+#***
+
+def test_play_correct_input_normal_turn():
+    #cg = ['e']
+    #wg = ['x']
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'elephant',
+                                                                cg = ['e'],
+                                                                wg = ['x'],
+                                                                new_guess = 'l',
+                                                                turns_left = 4)
+    assert cg == ['e', 'l']
+    assert wg == ['x']
+    assert turns_left == 4
+    assert game_over == False
+
+    
+def test_play_correct_input_win_turn():
+    #cg = ['d']
+    #wg = ['x']
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'dog',
+                                                                cg = ['d', 'o'],
+                                                                wg = ['x'],
+                                                                new_guess = 'g',
+                                                                turns_left = 4)
+    assert cg == ['d', 'o', 'g']
+    assert wg == ['x']
+    assert turns_left == 4
+    assert game_over == True
+    assert final_message == "You win!"
+
+
+def test_play_wrong_input_normal_turn():
+
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'dog',
+                                                                cg = ['d', 'o'],
+                                                                wg = ['x', 'l'],
+                                                                new_guess = 'l',
+                                                                turns_left = 3)
+    assert cg == ['d', 'o']
+    assert wg == ['x', 'l']
+    assert turns_left == 3
+    assert game_over == False
+
+def test_play_wrong_input_lose():
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'dog',
+                                                                cg = ['d', 'o'],
+                                                                wg = ['x','l','z','p'],
+                                                                new_guess = 'a',
+                                                                turns_left = 1)
+    assert cg == ['d', 'o']
+    assert wg == ['x','l','z','p','a']
+    assert turns_left == 0
+    assert game_over == True
+    assert final_message == "You lost! The word was 'dog'."
+
+
+def test_play_repeat_input_correct():
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'dog',
+                                                                cg = ['d','o'],
+                                                                wg = ['x'],
+                                                                new_guess = 'd',
+                                                                turns_left = 1)
+    assert cg == ['d', 'o']
+    assert wg == ['x']
+    assert turns_left == 1
+    assert game_over == False
+
+
+def test_play_repeat_input_wrong():    
+
+    cg, wg, turns_left, game_over, final_message = hangman.play(sw = 'dog',
+                                                                cg = ['d','o'],
+                                                                wg = ['x'],
+                                                                new_guess = 'x',
+                                                                turns_left = 1)
+    assert cg == ['d', 'o']
+    assert wg == ['x']
+    assert turns_left == 1
+    assert game_over == False
+
+    
+#***    
+
+
+
+#pytest -k mask_word test_hangman.py (-k will tell pytest which #test to run)
